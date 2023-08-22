f792ff110d631ced1728140d1a57659ffacbc261
Napster <rome.1205h@gmail.com>
2023-08-22 00:33:32 -0500

    137 update component styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import imagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media(min-width: 992px){
+    display: grid;
+    grid-template-columns: (2, 1fr)
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
 
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
-  color: #fff
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px; 
+  margin-button: 50px;
+  font-zise : 34px;
+
+  @media(min-width: 992px){
+    display: grid;
+    grid-template-columns: (2, 1fr)
+    column-gap: 2rem;
+  } 
 `
 
 function App() {
 
   return (
-    <Heading>
-      Hola Mundo
-    </Heading>
+    <Contenedor>
+      <Imagen
+        src={imagenCripto}
+        alt='Image kripto'
+      />
+      <Heading>Hola Mundo</Heading>
+    </Contenedor>
   )
 }
 
